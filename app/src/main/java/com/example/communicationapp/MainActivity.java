38ede759e62b85957a23aa1b7d5b63a93ed2f7b8
animesh288 <animesh.satish@gmail.com>
2021-10-03 13:55:09 +0530

    Main activity updated

--- app/src/main/java/com/example/communicationapp/MainActivity.java
+++ app/src/main/java/com/example/communicationapp/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.communicationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.communicationapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Connect.class));
+            }
+        });
+
+        binding.about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),About.class));
+            }
+        });
+
+        binding.departments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Departments.class));
+            }
+        });
+
+        binding.cutoffs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Cutoffs.class));
+            }
+        });
+
+        binding.tour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Tour.class));
+            }
+        });
+
+
+
     }
 }
