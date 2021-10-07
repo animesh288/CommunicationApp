4f4e9d1717f640d39ccc6e96dc6bba2d85b68faa
animesh288 <animesh.satish@gmail.com>
2021-10-07 10:43:51 +0530

    placement activity complete

--- app/src/main/java/com/example/communicationapp/Placement.java
+++ app/src/main/java/com/example/communicationapp/Placement.java
@@ -2,6 +2,7 @@ package com.example.communicationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,6 +18,32 @@ public class Placement extends AppCompatActivity {
 
         activityPlacementBinding=ActivityPlacementBinding.inflate(getLayoutInflater());
         setContentView(activityPlacementBinding.getRoot());
+
+        activityPlacementBinding.g1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Placement.this,chart21.class));
+            }
+        });
+        activityPlacementBinding.g2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Placement.this,chart20.class));
+            }
+        });
+        activityPlacementBinding.g3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Placement.this,chart19.class));
+            }
+        });
+        activityPlacementBinding.g4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Placement.this,chart18.class));
+            }
+        });
+
 
 
     }
