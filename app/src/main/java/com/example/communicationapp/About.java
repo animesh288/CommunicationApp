6a9f554968b0b590f7ca6eae87769bc0dd3aede2
animesh288 <animesh.satish@gmail.com>
2021-10-07 19:29:44 +0530

    about activity

--- app/src/main/java/com/example/communicationapp/About.java
+++ app/src/main/java/com/example/communicationapp/About.java
@@ -1,8 +1,13 @@
 package com.example.communicationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "2qc7Zdl_8tM";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/communicationapp/Cutoffs.java
+++ app/src/main/java/com/example/communicationapp/Cutoffs.java
@@ -2,13 +2,104 @@ package com.example.communicationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.communicationapp.databinding.ActivityCutoffsBinding;
 
 public class Cutoffs extends AppCompatActivity {
+
+    ActivityCutoffsBinding binding;
+    int state=1,cat=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cutoffs);
+        binding=ActivityCutoffsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.allindia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.allindia.setBackgroundColor(Color.CYAN);
+                binding.homestate.setBackgroundColor(Color.WHITE);
+                state=1;
+            }
+        });
+        binding.homestate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.homestate.setBackgroundColor(Color.CYAN);
+                binding.allindia.setBackgroundColor(Color.WHITE);
+                state=2;
+            }
+        });
+
+        binding.open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.open.setBackgroundColor(Color.CYAN);
+                binding.ews.setBackgroundColor(Color.WHITE);
+                binding.obc.setBackgroundColor(Color.WHITE);
+                binding.st.setBackgroundColor(Color.WHITE);
+                binding.sc.setBackgroundColor(Color.WHITE);
+                cat=1;
+
+            }
+        });
+        binding.ews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.open.setBackgroundColor(Color.WHITE);
+                binding.ews.setBackgroundColor(Color.CYAN);
+                binding.obc.setBackgroundColor(Color.WHITE);
+                binding.st.setBackgroundColor(Color.WHITE);
+                binding.sc.setBackgroundColor(Color.WHITE);
+                cat=2;
+
+            }
+        });
+        binding.obc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.open.setBackgroundColor(Color.WHITE);
+                binding.ews.setBackgroundColor(Color.WHITE);
+                binding.obc.setBackgroundColor(Color.CYAN);
+                binding.st.setBackgroundColor(Color.WHITE);
+                binding.sc.setBackgroundColor(Color.WHITE);
+                cat=3;
+
+            }
+        });
+        binding.st.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.open.setBackgroundColor(Color.WHITE);
+                binding.ews.setBackgroundColor(Color.WHITE);
+                binding.obc.setBackgroundColor(Color.WHITE);
+                binding.st.setBackgroundColor(Color.CYAN);
+                binding.sc.setBackgroundColor(Color.WHITE);
+                cat=4;
+
+            }
+        });
+        binding.sc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.open.setBackgroundColor(Color.WHITE);
+                binding.ews.setBackgroundColor(Color.WHITE);
+                binding.obc.setBackgroundColor(Color.WHITE);
+                binding.st.setBackgroundColor(Color.WHITE);
+                binding.sc.setBackgroundColor(Color.CYAN);
+                cat=5;
+
+            }
+        });
+
+
+
+
     }
 }
